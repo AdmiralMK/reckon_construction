41e7d2780795d037681c1a02ec8943f8b64652a1
AdmiralMK <mkfiles@mail.ru>
2022-02-28 02:44:51 +0500

    master

---
+++ windows/flutter/generated_plugin_registrant.cc
@@ -0,0 +1,17 @@
+//
+//  Generated file. Do not edit.
+//
+
+// clang-format off
+
+#include "generated_plugin_registrant.h"
+
+#include <window_manager/window_manager_plugin.h>
+#include <window_utils/window_utils.h>
+
+void RegisterPlugins(flutter::PluginRegistry* registry) {
+  WindowManagerPluginRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowManagerPlugin"));
+  WindowUtilsRegisterWithRegistrar(
+      registry->GetRegistrarForPlugin("WindowUtils"));
+}
